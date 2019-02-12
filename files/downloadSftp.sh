b8f4c5ee6c2faec649055cca767f760f04b30116
larahurtadojose <2774283+larahurtadojose@users.noreply.github.com>
2019-02-12 12:39:43 -0600

    Update downloadSftp.sh

--- files/downloadSftp.sh
+++ files/downloadSftp.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 #
-# This script require sshpass and sftp installed and configured first
+# Este script requiere sshpass sftp y ncat instalado y configurado primero
 #
-# Author: Miguel Soranno.
+# Author: Juan Lara
 #
 
 usr="$1"
 pass="$2"
 maquina="$3"
 componente="$6"
+proxy="$7"
+puerto="$8"
 
 export SSHPASS=$pass
-sshpass -e sftp -oBatchMode=no -b - $usr@$maquina << !
+sshpass -e sftp -oBatchMode=no -o ConnectTimeout=3 -o ProxyCommand='/usr/bin/ncat --proxy-type http --proxy $7:$8 %h %p' -oPort=22 -b - $usr@$maquina << !
 cd $4
 get $componente
 bye
